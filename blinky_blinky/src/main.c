911ccf042cc170f240472e68a336cd86bc9a6c61
jeffledin <ledin.jeffrey@gmail.com>
2017-07-22 21:27:16 -0500

    added TIM6 interrupt

--- blinky_blinky/src/main.c
+++ blinky_blinky/src/main.c
@@ -10,11 +10,7 @@
 
 
 #include "stm32f4xx.h"
-			
-#define PB9_MASK	0x0080;
-#define PB14_MASK	0x4000;
-
-#define TIM6_UIF	(TIM6->SR & (1 << 0))
+#include "defines.h"
 
 void initOSC(void);
 void initLEDs(void);
@@ -30,20 +26,20 @@ int main(void)
 
 	while(1)
 	{
-		if(count == 0)
-		{
-			++count;
-			GPIOB->ODR ^= PB9_MASK;
-		}
-		else if(count == 1)
-		{
-			--count;
-			GPIOB->ODR ^= PB14_MASK;
-			GPIOB->ODR ^= PB9_MASK;
-		}
-		while(!TIM6_UIF); // wait for counter to overflow and flag to be set
-		TIM6->SR &= (0 << 0); // reset TIM6 flag
-	}
+//		if(count == 0)
+//		{
+//			++count;
+//			GPIOB->ODR ^= PB9_MASK;
+//		}
+//		else if(count == 1)
+//		{
+//			--count;
+//			GPIOB->ODR ^= PB14_MASK;
+//			GPIOB->ODR ^= PB9_MASK;
+//		}
+//		while(!TIM6_UIF); // wait for counter to overflow and flag to be set
+//		TIM6->SR &= (0 << 0); // reset TIM6 flag
+	};
 }
 
 void initOSC(void)
@@ -57,9 +53,9 @@ void initLEDs(void)
 	//RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE); // TODO: investigate this syntax
 
 	RCC->AHB1ENR |= 0x2; // enable clock on Port B // THIS IS IMPORTANT
-	GPIOB->MODER = 0x10004000; // set PB7, PB14 as output
+	GPIOB->MODER = 0x10004001; // set PB0, PB7, PB14 as output
 	GPIOB->OTYPER = 0x00000000; // set all pins as push-pull (this is default anyways)
-	GPIOB->ODR = 0x00004080; // set PB7, PB14 high
+	GPIOB->ODR = 0x00004081; // set PB0, PB7, PB14 high
 
 	//	GPIO_InitTypeDef gpio;
 	//
@@ -74,12 +70,15 @@ void initLEDs(void)
 // TODO: Play around with timer interrupts
 void initTIM6(void)
 {
-	// Counter should overflow every 2s
+	// Counter should overflow every 1s
 	RCC->APB1ENR |= RCC_APB1ENR_TIM6EN; // enable clock for TIM6; taken from bit definitions provided in stm32f4xx.h for register APB1ENR
 	//RCC->APB1ENR |= (1 << 4); // equivalent to line above
 
 	TIM6->PSC = 0xFFFF; // prescaler of 1:65536
-	TIM6->ARR = 488; // period register -- clock seems to be 4 times faster than expected
+	TIM6->ARR = 244; // period register
 	TIM6->CNT = 0; // default of 0 for the counter
+	TIM6->DIER |= (1 << 0); // enable interrupts
+	TIM6->SR &= (0 << 0); // turn off interrupt flag
 	TIM6->CR1 |= (1 << 0); // turn on counter
+	NVIC_EnableIRQ(TIM6_IRQn); // enable the interrupt in the NVIC
 }
